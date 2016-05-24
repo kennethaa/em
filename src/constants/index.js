c17e4565bebf9291cbb22ecac570408c868044e4
Kenneth Aasan <k.aasan@sportradar.com>
2016-05-24 20:24:52 +0200

    Added google docs urls

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,9 @@
 export const TITLE = 'EM-KONKURRANSE 2016';
 
 export const API_ROOT = undefined;
+
+// Answers
+// https://spreadsheets.google.com/feeds/list/1UDVdgIPDobBaB8IHTD_x5oDozI6euWXfs_vLHem6u1I/default/public/values?alt=json
+
+// Results
+// https://spreadsheets.google.com/feeds/list/1z9_RyzvB2HYqGda3mw-fnJj8JhVivuuPXBHQLpWn__I/default/public/values?alt=json
